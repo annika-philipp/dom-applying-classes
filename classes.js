8fe2cad2f2e18484b064d7095f5dc83af84c23d9
Annika Philipp <annika@Annikas-MacBook-Air.local>
2017-12-23 11:49:12 +1300

    changed all 4 divs

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -32,4 +33,9 @@ function three () {
   three.classList.toggle('yellow')
 }
 
+
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')
+  makeVisible[0].classList.add('visible')
+}
